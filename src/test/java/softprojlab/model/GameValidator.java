64e094533d8743347bfb339127882dfb161f850a
Marton Pfemeter <pfemeter.marton@icloud.com>
2023-05-14 11:20:35 +0200

    adding basic test of generating a map

--- src/test/java/softprojlab/model/GameValidator.java
+++ src/test/java/softprojlab/model/GameValidator.java
@@ -1,29 +1,33 @@
 package softprojlab.model;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class GameValidator {
-
-    @BeforeAll
-    static void setUp() {
-
+    
+    private Game testDummy;
+    private final int VALID_NUMBER_OF_PLAYERS = 2;
+    private final int INVALID_NUMBER_OF_PLAYERS = 0;
+    
+    @BeforeEach
+    void setUp() {
+        this.testDummy = new Game();
     }
 
-    @ParameterizedTest
-    @MethodSource("getTestCase")
-    void test(String testInput, String testOutput) {
-        Game testDummy = new Game();
+    @Test
+    void generateThenStartNoExceptionTest() {
+        this.testDummy.generate(this.VALID_NUMBER_OF_PLAYERS, false);
         
-        String mockOutput = "";
-        
-        assertEquals(testOutput, mockOutput);
+        assertDoesNotThrow(this.testDummy::startGame);
     }
-
+    
+    @Test
+    void startWithoutGenerateThrowsExceptionTest() {
+        assertThrows(Exception.class, this.testDummy::startGame);
+    }
 
 }
